3693f743ff014130515076011a171d653abe5df2
Abigail Chelanagt <abigailchelah@gmail.com>
2025-05-01 20:35:26 +0300

    Add my files

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,5 +1,6 @@
-import "@testing-library/jest-dom";
+import React from "react";
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom'; 
 import Header from "../components/Header";
 
 test("renders a <header> element", () => {
